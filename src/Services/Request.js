fe6dcd22f90e1a9b05c6bedd110665325343c773
Darkkubik <45105081+Darkkubik@users.noreply.github.com>
2021-04-02 10:51:46 +0300

    Add files via upload

--- src/Services/Request.js
+++ src/Services/Request.js
@@ -1,6 +1,19 @@
 const Request = {
     getStudents() {
         return fetch("http://localhost:8080/student/all").then(res => res.json());
+    },
+    getJournal() {
+        return fetch("http://localhost:8080/journal/all").then(res => res.json());
+    },
+    getGroup() {
+        return fetch("http://localhost:8080/group/all").then(res => res.json());
+    },
+    getStudentOrder() {
+        return fetch("http://localhost:8080/student/all/order").then(res => res.json());
+    },
+    getJournalOrder() {
+        return fetch("http://localhost:8080/journal/all/order").then(res => res.json());
     }
+
 };
 export default Request
